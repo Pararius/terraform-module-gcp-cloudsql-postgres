145a21829de55e85cddb7d2a8eb7117cab190e20
Evy Bongers <28902567+EvyBongers@users.noreply.github.com>
2021-06-14 16:38:42 +0200

    Output instance IP addresses

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "ip_addresses" {
+  value = {
+    for addr in google_sql_database_instance.instance.ip_address : addr.type => addr.ip_address
+  }
+}
+
 output "passwords" {
   sensitive = true
   value     = { for user, pwd in random_password.sql_user : user => pwd.result }
