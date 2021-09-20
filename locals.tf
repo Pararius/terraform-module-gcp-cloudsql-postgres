d5358c07a320b65aa43960715d8a63a8f3ba5558
Evy Bongers <e.bongers@pararius.nl>
2021-09-20 10:20:28 +0200

    Always set password for database super user (#5)

--- locals.tf
+++ locals.tf
@@ -20,7 +20,7 @@ locals {
     # For every user, create a distict key in the format [user]@[host]
     # If the host parameter was omitted, use '%' as default and set the
     # default host in the resulting user object as well
-    for user in var.users : "${user.name}@${user.host == null ? "%" : user.host}" => defaults(user, { host = "%" })
+    for user in concat(var.users, [{ name = "root", host = "%" }]) : "${user.name}@${user.host == null ? "%" : user.host}" => defaults(user, { host = "%" })
   }
-  postgres_users = local.db_engine != "POSTGRES" ? {} : { for user in var.users : user.name => user }
+  postgres_users = local.db_engine != "POSTGRES" ? {} : { for user in concat(var.users, [{ name = "postgres" }]) : user.name => user }
 }
