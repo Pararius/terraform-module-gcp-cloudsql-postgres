ad9f2ae66828668a4e09281029cbed29611d98a0
Evy Bongers <e.bongers@pararius.nl>
2021-12-30 17:34:59 +0100

    Set default tier regardless of environment

--- locals.tf
+++ locals.tf
@@ -6,13 +6,12 @@ locals {
   default_labels = {
     env = var.environment
   }
-  default_tier = var.environment == "production" ? "db-custom-2-8192" : "db-f1-micro"
 
   backup_config         = defaults(var.backup_config, local.default_backup_config)
   externally_accessible = (var.externally_accessible || length(var.authorized_networks) > 0) ? true : false
   labels                = merge(local.default_labels, var.labels)
   storage_size          = var.storage_autoresize == true ? null : var.storage_size
-  tier                  = var.tier != null ? var.tier : local.default_tier
+  tier                  = var.tier != null ? var.tier : "db-f1-micro"
 
   admin_user = "postgres"
 }
