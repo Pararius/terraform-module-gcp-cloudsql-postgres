197bb658598fec722aa5f0d5cb9add90477eaf5c
Ruben Laban <5331085+tun0@users.noreply.github.com>
2022-01-03 17:22:14 +0100

    Fix resource naming (#7)

--- user.tf
+++ user.tf
@@ -2,7 +2,7 @@ resource "random_password" "admin_user" {
   length = 48
 }
 
-resource "google_sql_user" "postgres_user" {
+resource "google_sql_user" "admin_user" {
   instance = google_sql_database_instance.instance.name
 
   name     = local.admin_user
